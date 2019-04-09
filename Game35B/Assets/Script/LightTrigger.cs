406ce71ce5288290addf7de71e3ce8d6ed5f814b
ser_kaz97 <ser_kaz97@op.pl>
2019-04-09 17:54:13 +0200

    FloorSingleLightSwitch is going down if player enter, and going up if player exit

--- Game35B/Assets/Script/LightTrigger.cs
+++ Game35B/Assets/Script/LightTrigger.cs
@@ -9,23 +9,25 @@ public class LightTrigger : MonoBehaviour
     public Light l;
     // Start is called before the first frame update
     private float startYPos;
+    private bool isPlayerOn;
     void Start()
     {
         l.enabled = false;
         startYPos = gameObject.transform.position.y;
+        isPlayerOn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (l.enabled)
+        if (isPlayerOn)
         {
             if (gameObject.transform.position.y >= startYPos - 0.03)
             {
                 gameObject.transform.Translate(Vector3.down*Time.deltaTime*0.25f);
             }
         }
-        else if (!l.enabled)
+        else if (!isPlayerOn)
         {
             if (gameObject.transform.position.y < startYPos)
             {
@@ -36,11 +38,12 @@ public class LightTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        isPlayerOn = true;
         l.enabled = !l.enabled;
     }
 
-   /* private void OnTriggerExit(Collider other)
-    {
-        l.enabled = false;
-    }*/
+   private void OnTriggerExit(Collider other)
+   {
+       isPlayerOn = false;
+   }
 }
