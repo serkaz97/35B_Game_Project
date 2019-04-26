aeacf62dc774729c632721666a66d073497a4971
Matthew Moszczynski <matthew.moszczynski96@gmail.com>
2019-04-26 16:37:39 +0200

    Enemy Update #2
    Enemies are running towards specified target

--- Game35B/Assets/Script/Enemy.cs
+++ Game35B/Assets/Script/Enemy.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Transform _target;
+    private NavMeshAgent _agent;
+
     void Start()
     {
-        Debug.Log("I am an enemy!");
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        
+        if (_target)
+        {
+            _agent.SetDestination(_target.position);
+        }
     }
 }
