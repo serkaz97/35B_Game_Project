cdd70942eb808e99d77019a942872d13cce61dd9
serkaz97 <sergiusz.kazmierczak97@gmail.com>
2019-05-01 01:35:10 +0200

    added changes to firetrap

--- Game35B/Assets/FireActivator.cs
+++ Game35B/Assets/FireActivator.cs
@@ -4,62 +4,55 @@ using UnityEngine;
 
 public class FireActivator : MonoBehaviour
 {
-    public bool isPlayerOn;
-    public float Power;
-    private ParticleSystem ps;
+    private ParticleSystem flame;
     private AudioSource audioSource;
     private LifeController lc;
 
-    [SerializeField] private AudioClip fire;
+    public bool isPlayerOn;
+    public float Power = 5.0f;
+    public AudioClip fire;
 
-    // Start is called before the first frame update
     void Start()
     {
-        ps = gameObject.GetComponent<ParticleSystem>();
-        audioSource = gameObject.GetComponent<AudioSource>();      
-        ps.Stop();
+        flame = gameObject.GetComponent<ParticleSystem>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = fire;
         isPlayerOn = false;
-        Power = 5.0f;
+        audioSource.volume = 0f;
+        flame.Stop();
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator MuteSound()
     {
-      
+        isPlayerOn = false;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= Time.deltaTime * 0.5f;
+            yield return null;
+        }
+
+        flame.Stop();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         isPlayerOn = true;
-        ps.Play();
-        PlayFireSound();
+        flame.Play();
+        audioSource.Play();
+        audioSource.volume = 1.0f;
+        audioSource.loop = true;
+
         lc = GameObject.FindWithTag("PlayerStats").GetComponent<LifeController>();
     }
 
     private void OnTriggerExit(Collider other)
     {
-        isPlayerOn = false;
-        ps.Stop();
-        StopFireSound();
-        //audioSource.Stop();
-        
+        StartCoroutine(MuteSound());
     }
 
     private void OnTriggerStay(Collider other)
     {
         lc.setDamage(Power * Time.deltaTime);
-        
-    }
-
-    private void PlayFireSound()
-    {
-        audioSource.clip = fire;
-        audioSource.Play();
-    }
-
-    private void StopFireSound()
-    {
-        audioSource.clip = fire;
-        audioSource.Stop();
     }
 }
