599bee311c111c778d6b80e1555a106d20e4e143
serkaz97 <sergiusz.kazmierczak97@gmail.com>
2019-05-01 18:48:06 +0200

    recreated floor spike trap

--- Game35B/Assets/SpikesUp.cs
+++ Game35B/Assets/SpikesUp.cs
@@ -28,12 +28,12 @@ public class SpikesUp : MonoBehaviour
         float up = 3;
         while (gameObject.transform.localPosition.y < startPos.y + up)
         {
-            gameObject.transform.Translate(Vector3.forward * 6f * Time.deltaTime);
+            gameObject.transform.Translate(Vector3.up * 6f * Time.deltaTime);
             yield return null;
         }
         while (gameObject.transform.localPosition.y > startPos.y)
         {
-            gameObject.transform.Translate(Vector3.forward * -1.5f * Time.deltaTime);
+            gameObject.transform.Translate(Vector3.up * -1.5f * Time.deltaTime);
             yield return null;
         }
 
