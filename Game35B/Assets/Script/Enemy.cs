01a1b9185fa952e7d4d33374d1c644a0d43f5c1b
Matthew Moszczynski <matthew.moszczynski96@gmail.com>
2019-06-27 03:29:42 +0200

    Enemy Update #2
    - Updated enemy
    - Added Push and Pull powers
    - Added 2 new animations to player's hands

--- Game35B/Assets/Script/Enemy.cs
+++ Game35B/Assets/Script/Enemy.cs
@@ -1,16 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Transform _target;
+
+    private NavMeshAgent _agent;
+    private Rigidbody _rb;
+
+
     void Start()
     {
-        Debug.Log("I am an enemy!");
+        _agent = GetComponent<NavMeshAgent>();
+        _rb = GetComponent<Rigidbody>();
+        _rb.isKinematic = true;
+    }
+
+    private IEnumerator Pause()
+    {
+        yield return new WaitForSeconds(1.3f);
+        _rb.isKinematic = true;
+        _agent.enabled = true;
     }
 
     void Update()
     {
-        
+        if (_target)
+        {
+            if(Vector3.Distance(transform.position, _target.position) > 3f)
+            {
+                _agent.SetDestination(_target.position);
+                _agent.isStopped = false;
+            }
+            else
+            {
+                _agent.isStopped = true;
+            }
+        }
+    }
+
+    public void Push()
+    {
+        Vector3 direction = transform.position - GameManager.Instance.GetPlayer().transform.position;
+        direction.Normalize();
+        _rb.isKinematic = false;
+        _agent.enabled = false;
+        _rb.AddForce(direction * 7f, ForceMode.Impulse);
+        StartCoroutine(Pause());
+    }
+
+    public void Pull()
+    {
+        Vector3 direction = GameManager.Instance.GetPlayer().transform.position - transform.position;
+        direction.Normalize();
+        _rb.isKinematic = false;
+        _agent.enabled = false;
+        _rb.AddForce(direction * 7f, ForceMode.Impulse);
+        StartCoroutine(Pause());
+    }
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (obj.transform.CompareTag("Player"))
+        {
+            _target = obj.transform;
+        }
     }
 }
---
+++ Game35B/Assets/Script/PlayerPowers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPowers : MonoBehaviour
+{
+    public void PushEnemy()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+
+            if (hit.transform.CompareTag("Enemy"))
+            {
+                hit.transform.GetComponent<Enemy>().Push();
+            }
+        }
+    }
+
+    public void PullEnemy()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+
+            if (hit.transform.CompareTag("Enemy"))
+            {
+                hit.transform.GetComponent<Enemy>().Pull();
+            }
+        }
+    }
+}
